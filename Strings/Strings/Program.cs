2d4cc4578d1b6f4fdf4a03c7d00f1fcb76c3f77d
phamd2 <phamd@stpaulsschool.org.uk>
2025-10-07 08:59:36 +0100

    Strings Homework Q1

--- Strings/Strings/Program.cs
+++ Strings/Strings/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+
+string mexicanwave(string str)
+{
+    var result = new List<string>();
+
+    for (int i = 0; i < str.Length; i++)
+    {
+        if (char.IsWhiteSpace(str[i]))
+            continue;
+
+        char[] chars = str.ToCharArray();
+        chars[i] = char.ToUpper(chars[i]);
+        result.Add(new string(chars));
+    }
+
+    return string.Join(", ", result);
+}
+
