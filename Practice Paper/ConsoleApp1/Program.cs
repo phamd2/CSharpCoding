201da1e1404848a54ebbb8736f5974a10b4485c3
phamd2 <phamd@stpaulsschool.org.uk>
2026-01-14 10:15:08 +0000

    code

---
+++ Practice Paper/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿void compressed()
+{
+    Console.WriteLine("Enter your string");
+    string original = Console.ReadLine();
+    char current = 'A';
+    int total = 0;
+    foreach (char c in original)
+    {
+        if (c == current)
+            total++;
+        else
+        {
+            Console.Write(Convert.ToString(current) + total+ ' ');
+            current = c;
+            total = 1;
+        }
+
+    }
+    Console.Write(Convert.ToString(current) + total + ' ');
+}
+compressed();
